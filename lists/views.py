4931c9228e051eed027dc9909489c5ce3640b473
Vladislav Shipovskoi <sgipovskoi@gmail.com>
2018-02-12 11:13:20 +0700

    add base html

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 from .models import Todo
+
+
+def todo_detail(request, pk):
+    todos = get_object_or_404(Todo, pk=pk)
+    return render(request, 'todolist/todo_detail.html', {'todos': todos})
+
+def index(request):
+    return render(request, 'todolist/base.html')
 
 
 def todo_list(request):
