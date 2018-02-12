56f80f92e4a84fb174238aa6ef0e32ded3d4f01a
Vladislav Shipovskoi <sgipovskoi@gmail.com>
2018-02-12 09:45:57 +0700

    add todo model, simple html and css

--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Todo
 
-# Create your views here.
+
+def todo_list(request):
+    todos = Todo.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+    return render(request, 'todolist/todo_list.html', {'todos': todos})
