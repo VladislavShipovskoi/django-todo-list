fa5a8d1a24b350428072c2ddf20246b56462f8cf
Vladislav Shipovskoi <sgipovskoi@gmail.com>
2018-02-14 16:11:58 +0700

    change template login and css

--- todolist/settings.py
+++ todolist/settings.py
@@ -116,7 +116,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR,'static/')
 STATIC_ROOT = os.path.join(BASE_DIR, 'lists/static')
 LOGIN_URL='/login'
 LOGIN_REDIRECT_URL='/'
